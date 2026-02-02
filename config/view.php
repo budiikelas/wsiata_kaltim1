c41ba2c13fe435e91eb1ce9f523036e513cedbbc
Hasumiii <hilalsulthanuladzam@gmail.com>
2026-02-02 17:42:52 +0800

    aku lupa ganti branch lagi, detail.blade.php kuubah biar ga pake data dummy, jadi udah kusambungin ke datbackend

--- config/view.php
+++ config/view.php
@@ -3,14 +3,8 @@
 return [
 
     /*
-    |--------------------------------------------------------------------------
     | View Storage Paths
-    |--------------------------------------------------------------------------
-    |
-    | Most templating systems load templates from disk. Here you may specify
-    | an array of paths that should be checked for your views. Of course
-    | the usual Laravel view path has already been registered for you.
-    |
+
     */
 
     'paths' => [
@@ -18,14 +12,7 @@ return [
     ],
 
     /*
-    |--------------------------------------------------------------------------
     | Compiled View Path
-    |--------------------------------------------------------------------------
-    |
-    | This option determines where all the compiled Blade templates will be
-    | stored for your application. Typically, this is within the storage
-    | directory. However, as usual, you are free to change this value.
-    |
     */
 
     'compiled' => env(
--- routes/web.php
+++ routes/web.php
@@ -7,48 +7,55 @@ use App\Http\Controllers\DestinationController;
 use App\Http\Controllers\FacilityController;
 use App\Http\Controllers\GalleryController;
 use App\Http\Controllers\ReviewController;
-
 use App\Http\Controllers\LandingController;
 
+// =====================
+// Public Routes
+// =====================
+Route::get('/', [LandingController::class, 'index'])->name('home');
 
-//  Public Routes
+// Detail destinasi (PUBLIC, biar user bisa lihat)
+Route::get('/detail/{id}', [DestinationController::class, 'show'])
+    ->name('destinations.detail');
 
-Route::get('/', [LandingController::class, 'index']);
-
-// Authentication Access
+// =====================
+// Authentication
+// =====================
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
 Route::post('/register', [AuthController::class, 'register']);
 
-/*
-
-Protected routes (harus login)
-
-*/
+// =====================
+// Protected Routes
+// =====================
 Route::middleware('auth')->group(function () {
+
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-    
+
     // Pages
     Route::get('/packages', function () {
         return view('packages');
     });
 
-    Route::get('/detail', function () {
-        return view('detail');
-    });
-
-    // Resource Management
+    // Resource Management (CRUD)
     Route::resource('categories', CategoryController::class);
     Route::resource('destinations', DestinationController::class);
     Route::resource('facilities', FacilityController::class);
+
     Route::post('galleries', [GalleryController::class, 'store']);
     Route::delete('galleries/{id}', [GalleryController::class, 'destroy']);
+
     Route::post('reviews', [ReviewController::class, 'store']);
 
-    // Admin Access
+    // =====================
+    // Admin Routes
+    // =====================
     Route::prefix('admin')->middleware('admin')->group(function () {
-        Route::get('/', [\App\Http\Controllers\AdminController::class, 'index'])->name('admin.dashboard');
-        Route::get('/destinations', [\App\Http\Controllers\AdminController::class, 'destinations'])->name('admin.destinations');
+        Route::get('/', [\App\Http\Controllers\AdminController::class, 'index'])
+            ->name('admin.dashboard');
+
+        Route::get('/destinations', [\App\Http\Controllers\AdminController::class, 'destinations'])
+            ->name('admin.destinations');
     });
 });
